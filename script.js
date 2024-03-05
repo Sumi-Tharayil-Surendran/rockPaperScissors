2768e97db6a0d81fe514a03a97b43866f6b09a69
Sumi <sumi.tharayil@gmail.com>
2024-03-05 19:00:20 +0000

    Add function to enter valid username

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
 //disappear rules when user click on the close button
-function rules(){
+function rules() {
     let main = document.querySelector(".main");
-    main.style.display='none'
+    main.style.display = 'none'
     let heading = document.getElementById("heading")
-    heading.style.display='none'
+    heading.style.display = 'none'
     let userlogin = document.querySelector(".userlogin")
-    userlogin.style.display="block";
+    userlogin.style.display = "block";
 }
+let myButton1 = document.getElementById("myButton1");
+let error = document.getElementById("error")
 
+myButton1.addEventListener('click', function (event) {
+    let userName = document.getElementById("user").value.trim();
+    if (!userName || !/^[a-zA-Z]+$/.test(userName)) {
+        error.textContent="Enter a valid name";
+        event.preventDefault();
+    } else {
+        error.textContent="${username}"+ "good to go"
+    }
+
+
+});
+
+
+// let myButton1 = document.getElementById("myButton1");
+
+// myButton1.addEventListener('click', function(event) {
+//     let userName = document.getElementById("user").value.trim();
+//     if (!userName || !/^[a-zA-Z]+$/.test(userName)) {
+//         window.alert("Enter a valid name");
+//         event.preventDefault();
+//     } else {
+//         alert('Welcome, ' + userName + '! You may proceed.');
+//     }
+// });
