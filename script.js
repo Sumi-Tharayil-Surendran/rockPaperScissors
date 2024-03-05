86713c453188fe36176b2a3ff05726c2bf2f3503
Sumi <sumi.tharayil@gmail.com>
2024-03-05 13:54:53 +0000

    Add start function

---
+++ script.js
@@ -0,0 +1,7 @@
+//disappear rules when user click on the close button
+function rules(){
+    let main = document.querySelector(".main");
+    main.style.display='none'
+    let heading = document.getElementById("heading")
+    heading.style.display='none'
+}
